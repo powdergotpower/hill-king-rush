24b939816e1b87d9de6192d2fcb90104526538ee
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-10-24 20:54:03 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.a57f006466954800bb6b00c38318324e',
   appName: 'Rock King',
   webDir: 'dist',
-  server: {
-    url: 'https://a57f0064-6695-4800-bb6b-00c38318324e.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   android: {
     allowMixedContent: true
   }
